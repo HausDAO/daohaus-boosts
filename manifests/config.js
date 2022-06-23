4f7d641c152c7c654623dcb1d706e58e77b1c5f2
Brian Rossetti <brossetti1@gmail.com>
2022-06-23 14:59:40 -0400

    remove-rinkeby-kovan

--- manifests/config.js
+++ manifests/config.js
@@ -61,40 +61,6 @@ module.exports.config = {
     ],
     templates: [],
   },
-  kovan: {
-    dataSources: [
-      {
-        name: "transmutation",
-        template: "transmutation-ds.yaml",
-        address: "0x46A8E5b4B15b81b14AFf10Ac220c5A7B13E8282A",
-        startBlock: 20544190,
-      },
-      {
-        name: "transmutationV2",
-        template: "transmutationV2-ds.yaml",
-        address: "0xbca622291fFe797C77a8Bc6D000584b22877e971",
-        startBlock: 23758392,
-      },
-      {
-        name: "wrapNZap",
-        template: "wrapNZap-ds.yaml",
-        address: "0xbf9e327d465A4A160fA7805282Fb8C7aB892770a",
-        startBlock: 24527390,
-      },
-      {
-        name: "molochToken",
-        template: "molochToken-ds.yaml",
-        address: "0x2298cc330a4c359aea3583ddd81bacfab53d7da5",
-        startBlock: 31512182,
-      },
-    ],
-    templates: [
-      {
-        name: "transmutation",
-        template: "transmutation-template.yaml",
-      },
-    ],
-  },
   xdai: {
     dataSources: [
       {
@@ -128,23 +94,6 @@ module.exports.config = {
         template: "transmutation-template.yaml",
       },
     ],
-  },
-  rinkeby: {
-    dataSources: [
-      {
-        name: "wrapNZap",
-        template: "wrapNZap-ds.yaml",
-        address: "0x4e521FF388c83b4c945a33984ba42Efb73Cc04e6",
-        startBlock: 8493415,
-      },
-      {
-        name: "molochToken",
-        template: "molochToken-ds.yaml",
-        address: "0x12d1a14c06c3b75e541ec7e30a87e13beeda2965",
-        startBlock: 10647267,
-      },
-    ],
-    templates: [],
   },
   goerli: {
     dataSources: [
